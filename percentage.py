f9d2908be7b07ea9b1f666542b221483becbaa90
Gituser143 <bhargavsnv100@gmail.com>
2020-04-11 22:59:30 +0530

    Made sectors for each pincode

--- percentage.py
+++ percentage.py
@@ -8,7 +8,7 @@ images_folder_path = os.path.abspath(os.path.join('generated')) + '/'
 generated_folder_path = os.path.abspath(os.path.join('masks')) + '/'
 images_list = os.listdir(images_folder_path)
 images = [i.split('.')[0] for i in images_list]
-
+print(len(images))
 lower_green = np.array([40, 0, 0])
 upper_green = np.array([100, 255, 255])
 
@@ -17,24 +17,33 @@ if os.path.exists("data.txt"):
 else:
   print("The file 'data.txt' does not exist")
 
+x = 1029
+y = 690
+
+location = [["NW", "N", "NE"],["W", "C", "E"],["SW", "S", "SE"]]
+
 with open("data.txt", "a") as file_object:
 
     for image in images:
 
         img = cv2.imread(images_folder_path + image + '.png')
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        maskg = cv2.inRange(hsv, lower_green, upper_green)
-        cv2.imwrite(generated_folder_path + image + '.png', maskg)
+        #cv2.imwrite(generated_folder_path + image + '.png', maskg)
+        for i in range(0,3):
+            for j in range(0,3):
 
-        green_pixels = 0
-        total_pixels = img.shape[0] * img.shape[1]
+                sector = img[int(x*j/3):int(x*(j+1)/3), int(y*i/3):int(y*(i+1)/3)]
+                hsv = cv2.cvtColor(sector, cv2.COLOR_BGR2HSV)
+                maskg = cv2.inRange(hsv, lower_green, upper_green)
 
-        for i in range(0, maskg.shape[0]):
-            for j in range(0,maskg.shape[1]):
-                if maskg[i,j] == 255:
-                    green_pixels += 1
+                green_pixels = 0
+                total_pixels = sector.shape[0] * sector.shape[1]
 
-        perc = round( (green_pixels / total_pixels * 100) , 2)
-        
-        file_object.write(image + "," + str(perc) + "\n")
+                for a in range(0, maskg.shape[0]):
+                    for b in range(0,maskg.shape[1]):
+                        if maskg[a,b] == 255:
+                            green_pixels += 1
+
+                perc = round( (green_pixels / total_pixels * 100) , 2)
+                file_object.write(image + "-" + location[i][j] + "," + str(perc) + "\n")
+                print(image + "-" + location[i][j] + "," + str(perc) + "\n")
