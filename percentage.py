cb281e15e732eb1b30613f22f79618b4022abb8c
Gituser143 <bhargavsnv100@gmail.com>
2020-04-11 19:22:17 +0530

    Remove redundant lists and comments in percentage.py

--- percentage.py
+++ percentage.py
@@ -11,7 +11,6 @@ images = [i.split('.')[0] for i in images_list]
 
 lower_green = np.array([40, 0, 0])
 upper_green = np.array([100, 255, 255])
-green_pixel = []
 
 if os.path.exists("data.txt"):
   os.remove("data.txt")
@@ -21,20 +20,21 @@ else:
 with open("data.txt", "a") as file_object:
 
     for image in images:
+
         img = cv2.imread(images_folder_path + image + '.png')
-        #cv2.imshow('str', img)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
         maskg = cv2.inRange(hsv, lower_green, upper_green)
         cv2.imwrite(generated_folder_path + image + '.png', maskg)
-        green_pixel = []
+
+        green_pixels = 0
+        total_pixels = img.shape[0] * img.shape[1]
+
         for i in range(0, maskg.shape[0]):
             for j in range(0,maskg.shape[1]):
                 if maskg[i,j] == 255:
-                    green_pixel.append([i,j])
+                    green_pixels += 1
 
-        total_pixels = img.shape[0] * img.shape[1]
-        green_pixels = len(green_pixel)
         perc = round( (green_pixels / total_pixels * 100) , 2)
-        print(image + "," + str(perc))
-        file_object.write(image + "," + str(perc))
-        file_object.write("\n")
+        
+        file_object.write(image + "," + str(perc) + "\n")
