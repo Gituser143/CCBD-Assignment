551d8705d4ada9a7a314355ac582a7db89783bfd
Gituser143 <bhargavsnv100@gmail.com>
2020-04-12 14:57:07 +0530

    Put checking condition in map and remove from reduce

--- mapreduce.py
+++ mapreduce.py
@@ -6,16 +6,17 @@ class checkForGreen(MRJob):
 	#l = dict()
 
 	def steps(self):
-		return [MRStep(mapper=self.mapper_get_green,reducer=self.reducer_check_green)]
+		return [MRStep(mapper=self.mapper_check_green,reducer=self.reducer_get_green)]
 
-	def mapper_get_green(self, _, line):
+	def mapper_check_green(self, _, line):
 		(pincode, percentage) = line.split(',')
-		p = round(float(percentage), 1)
-		yield pincode + ' : ' + str(p), float(percentage)
+		percentage = float(percentage)
+		if(percentage > 60):
+			p = round(percentage, 1)
+			yield pincode, percentage
 
-	def reducer_check_green(self, key, values):
-		if(sum(values) > 30) :
-			yield key, sum(values)
+	def reducer_get_green(self, key, values):
+		yield key, sum(values)
 
 if __name__ == '__main__':
 	checkForGreen.run()
