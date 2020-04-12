63a9f02a80f765d3fe1f8b6b99f036be8a7f89e8
Gituser143 <bhargavsnv100@gmail.com>
2020-04-12 15:43:18 +0530

    Rname green to masks.py

--- masks.py
+++ masks.py
@@ -5,15 +5,15 @@ import numpy as np
 
 
 images_folder_path = os.path.abspath(os.path.join('generated')) + '/'
-generated_folder_path = os.path.abspath(os.path.join('color')) + '/'
+generated_folder_path = os.path.abspath(os.path.join('masks')) + '/'
 images = os.listdir(images_folder_path)
 
-lower_green = np.array([40, 0, 0]) 
-upper_green = np.array([100, 255, 255]) 
+lower_green = np.array([40, 0, 0])
+upper_green = np.array([100, 255, 255])
 
 
 for image in images:
     img = cv2.imread(images_folder_path + image)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
-    maskg = cv2.inRange(hsv, lower_green, upper_green) 
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    maskg = cv2.inRange(hsv, lower_green, upper_green)
     cv2.imwrite(generated_folder_path + image, maskg)
