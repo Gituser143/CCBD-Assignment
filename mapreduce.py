2de1d8366507da63be50389a56da4d4cb3e9ee96
Gituser143 <bhargavsnv100@gmail.com>
2020-04-12 15:50:15 +0530

    Clean mapreduce and rename green.py to masks.py

--- mapreduce.py
+++ mapreduce.py
@@ -1,20 +1,19 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-
 class checkForGreen(MRJob):
-	#l = dict()
 
 	def steps(self):
-		return [MRStep(mapper=self.mapper_get_green,reducer=self.reducer_check_green)]
+		return [MRStep(mapper = self.mapper_get_green, reducer = self.reducer_check_green)]
 
 	def mapper_get_green(self, _, line):
 		(pincode, percentage) = line.split(',')
-		p = round(float(percentage), 1)
-		yield pincode + ' : ' + str(p), float(percentage)
+		percentage = float(percentage)
+		if(percentage > 30) :
+			yield pincode, float(percentage)
 
 	def reducer_check_green(self, key, values):
-		if(sum(values) > 30) :
+
 			yield key, sum(values)
 
 if __name__ == '__main__':
