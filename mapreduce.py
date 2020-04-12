de4cf4085af82948cfbbf958a155c5a20e54e39e
Gituser143 <bhargavsnv100@gmail.com>
2020-04-12 15:37:38 +0530

    Clean mapreduce.py

--- mapreduce.py
+++ mapreduce.py
@@ -1,18 +1,15 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-
 class checkForGreen(MRJob):
-	#l = dict()
 
 	def steps(self):
-		return [MRStep(mapper=self.mapper_check_green,reducer=self.reducer_get_green)]
+		return [MRStep(mapper = self.mapper_check_green, reducer = self.reducer_get_green)]
 
 	def mapper_check_green(self, _, line):
 		(pincode, percentage) = line.split(',')
 		percentage = float(percentage)
 		if(percentage > 60):
-			p = round(percentage, 1)
 			yield pincode, percentage
 
 	def reducer_get_green(self, key, values):
